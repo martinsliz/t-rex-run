cfa0160e445d22470f983c3d6ff5ce1ab4a16d37
Liz Martins <elizabeth.martins5@gmail.com>
2024-07-27 21:04:49 -0400

    function so trex can jump up when space bar is pressed

--- App.js
+++ App.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   const dino = document.querySelector('.dino')
-  console.log(dino)
+  const grid = document.querySelector('.grid')
+  let gravity = 0.9
+  isJumping = false
+
+  const control = (event) => {
+    if (event.key === ' ') {
+      if (!isJumping) {
+        jump()
+      }
+    }
+  }
+
+  let position = 0
+  const jump = () => {
+    let timerId = setInterval(function () {
+      position += 20
+      position = position * gravity
+      dino.style.bottom = position + 'px'
+    }, 20)
+  }
+
+  document.addEventListener('keydown', control)
 })
