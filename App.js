ab599a8ea5e460b578ff54d67125a00bcd0dc07c
Liz Martins <elizabeth.martins5@gmail.com>
2024-07-28 21:26:31 -0400

    logic to implement jumping, creating an instance of an obstacle, and game over when trex and obstacle touch

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const dino = document.querySelector('.dino')
   const grid = document.querySelector('.grid')
+  const alert = document.getElementById('alert')
   let gravity = 0.9
-  isJumping = false
+  let isJumping = false
+  let isGameOver = false
 
   const control = (event) => {
     if (event.key === ' ') {
@@ -14,12 +16,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let position = 0
   const jump = () => {
-    let timerId = setInterval(function () {
+    isJumping = true
+    let count = 0
+    let timerId = setInterval(() => {
+      // move down
+      if (count === 11) {
+        clearInterval(timerId)
+        let downTimerId = setInterval(() => {
+          if (count === 0) {
+            clearInterval(downTimerId)
+            isJumping = false
+          }
+          console.log('going down')
+          position -= 5
+          count--
+          position = position * gravity
+          dino.style.bottom = position + 'px'
+        }, 20)
+      }
+      // move up
       position += 20
+      count++
+      console.log(count)
       position = position * gravity
       dino.style.bottom = position + 'px'
     }, 20)
   }
+
+  //generating obstacles
+  const generateObstacle = () => {
+    let obstaclePosition = 1000
+    const obstacle = document.createElement('div')
+    obstacle.classList.add('obstacle')
+    grid.appendChild(obstacle)
+    obstacle.style.left = obstaclePosition + 'px'
+
+    let timerId = setInterval(() => {
+      if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+        clearInterval(timerId)
+        alert.innerHTML = 'Game Over'
+        isGameOver = true
+        // remove all children
+        while (grid.firstChild) {
+          grid.removeChild(grid.lastChild)
+        }
+      }
+      obstaclePosition -= 10
+      obstacle.style.left = obstaclePosition + 'px'
+    }, 20)
+  }
+  generateObstacle()
 
   document.addEventListener('keydown', control)
 })
